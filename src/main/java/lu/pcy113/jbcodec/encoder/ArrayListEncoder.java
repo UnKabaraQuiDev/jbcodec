c1750212a62cb845744d4c9cc34c2c59399c8a08
UnKabaraQuiDev <unkabaraquidev@github.com>
2024-07-07 17:56:47 +0200

    Updated ArrayListD/Encoder

--- src/main/java/lu/pcy113/jbcodec/encoder/ArrayListEncoder.java
+++ src/main/java/lu/pcy113/jbcodec/encoder/ArrayListEncoder.java
@@ -3,28 +3,41 @@ package lu.pcy113.jbcodec.encoder;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
-
-import lu.pcy113.pclib.PCUtils;
+import java.util.stream.IntStream;
 
 public class ArrayListEncoder extends DefaultObjectEncoder<ArrayList<?>> {
+
+	public ArrayListEncoder() {
+		super(ArrayList.class);
+	}
 
 	/**
 	 * ( HEAD 2b - SIZE 4b - SUB HEAD 2b - DATA xb
 	 */
 	@Override
 	public ByteBuffer encode(boolean head, ArrayList<?> obj) {
-		List<Byte> elements = new ArrayList<>();
+		List<ByteBuffer> elements = new ArrayList<>();
 		for (Object o : obj) {
-			elements.addAll(PCUtils.byteArrayToList(cm.encode(true, o).array()));
+			elements.add(cm.encode(true, o));
 		}
-		ByteBuffer bb = ByteBuffer.allocate((head ? 2 : 0) + 4 + elements.size());
-		if (head)
-			bb.putShort(header);
+
+		ByteBuffer bb = ByteBuffer.allocate(estimateSize(head, obj));
+
+		super.putHeader(head, bb);
+
 		bb.putInt(obj.size());
-		bb.put(PCUtils.byteListToPrimitive(elements));
+
+		for (ByteBuffer bbb : elements) {
+			bb.put(bbb);
+		}
 
 		bb.flip();
 		return bb;
+	}
+
+	@Override
+	public int estimateSize(boolean head, ArrayList<?> obj) {
+		return super.estimateHeaderSize(head) + 4 + IntStream.range(0, obj.size()).map(c -> cm.estimateSize(true, obj.get(c))).sum();
 	}
 
 }
